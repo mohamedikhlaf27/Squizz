2961bf689c9419560274610b39fbe7cda38f4aa2
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2020-09-10 10:03:13 +0200

    test

--- src/main/java/example/Squizz/SquizzApplication.java
+++ src/main/java/example/Squizz/SquizzApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SquizzApplication {
-
+//comment
 	public static void main(String[] args) {
 		SpringApplication.run(SquizzApplication.class, args);
 	}
