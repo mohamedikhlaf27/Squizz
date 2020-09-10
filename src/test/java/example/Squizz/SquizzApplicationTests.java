7ed9d6f38482f0fa325dbcdb5f659701d6bce9c9
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2020-09-10 10:02:32 +0200

    Initial commit

---
+++ src/test/java/example/Squizz/SquizzApplicationTests.java
@@ -0,0 +1,13 @@
+package example.Squizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
